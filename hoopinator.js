a785d01d8478ae70c88e8a329481cfc5c34249c7
NobiDevs <67208310+NobiDevs@users.noreply.github.com>
2024-10-12 23:03:37 -0500

    update hoopinator
    - major update
    - committing for progress

--- hoopinator.js
+++ hoopinator.js
@@ -51,6 +51,40 @@ function getA(prompt) {
     }
 }
 
+function temp() {
+    return {
+        getItem(key) {
+            let res = JSON.parse(window.localStorage.getItem(key));
+            if (res) {
+                if (res.expireTime <= Date.now()) {
+                    window.localStorage.removeItem(key);
+                    return null;
+                }
+                return res.data;
+            }
+            return null;
+        },
+        setItem(key, value, time) {
+            time *= 60000;
+            let res = {
+                data: value
+            };
+            if (time) {
+                res.expireTime = Date.now() + time;
+            }
+            window.localStorage.setItem(key, JSON.stringify(res));
+        },
+        removeItem(key) {
+            window.localStorage.removeItem(key);
+        },
+        clear() {
+            window.localStorage.clear();
+        }
+    };
+}
+
+window.temp = temp();
+
 function y(h, vy, a, l) {
     var s = (a * (Math.pow(h, 2)) / 2);
     return (((vy * h) + s) + l) * 100;
@@ -62,61 +96,66 @@ function x(h, vx) {
 
 function getRes(xv, yv) {
     const tb = `
-    <table id="res-table">
+    <table id="res-table" class="styled-table">
         <tr>
             <th></th>
             <th>Δx<br> (cm)</th>
             <th>Δy<br> (cm)</th>
         </tr>
         <tr>
-            <th>H1</th>
+            <th><sup>1</sup>&frasl;<sub>3</sub></th>
             <td>${xv[0].toFixed(2)}</td>
             <td>${yv[0].toFixed(2)}</td>
         </tr>
         <tr>
-            <th>H2</th>
+            <th><sup>1</sup>&frasl;<sub>2</sub></th>
             <td>${xv[1].toFixed(2)}</td>
             <td>${yv[1].toFixed(2)}</td>
         </tr>
         <tr>
-            <th>H3</th>
+            <th><sup>3</sup>&frasl;<sub>4</sub></th>
             <td>${xv[2].toFixed(2)}</td>
             <td>${yv[2].toFixed(2)}</td>
         </tr>
     </table>
     `;
-
     return tb;
 }
 
 function getInp(v, d, a, l) {
     const tb = `
-    <table id="input-table">
+    <table id="input-table" class="styled-table">
         <tr>
             <th></th>
             <th>V<sub>i</sub></th>
             <th>&#952;</th>
             <th>a</th>
-            <th>&#8616;</th>
+            <th>&#8613;</th>
         </tr>
         <tr>
-            <th>input</th>
+            <th>Input</th>
             <td>${v}</td>
-            <td>${d}</td>
+            <td>${d}&deg;</td>
             <td>${a}</td>
-            <td>${l}</td>
+            <td>${l}m</td>
         </tr>
     </table>
     `;
-
     return tb;
 }
 
+const res = document.getElementById('res-table');
+const inp = document.getElementById('input-table');
+
+let v, d, a, l;
+let xv, yv;
+let loadTime;
+
 function hoopinate() {
-    var v = getFloat('Initial Velocity? (m/s)');
-    var d = getInt('Launcher Angle?');
-    var a = getA('Acceleration?');
-    var l = getFloat('Launcher Height? (cm)') / 100;
+    v = getFloat('Initial Velocity? (m/s)');
+    d = getInt('Launcher Angle?');
+    a = getA('Acceleration?');
+    l = getFloat('Launcher Height? (cm)') / 100;
 
     var r = d * (Math.PI / 180);
 
@@ -129,14 +168,91 @@ function hoopinate() {
     var hf = t * (1 / 2);
     var qt = t * (3 / 4);
 
-    let xv = [th, hf, qt].map(h => x(h, vx));
-    let yv = [th, hf, qt].map(h => y(h, vy, a, l));
+    xv = [th, hf, qt].map(h => x(h, vx));
+    yv = [th, hf, qt].map(h => y(h, vy, a, l));
 
-    const res = document.getElementById('res-table');
-    const inp = document.getElementById('input-table');
+    load(true)
+}
+
+
+function load(c) {
+    const element = document.getElementById('load')
+    loadTime = Math.random() * (5000 - 2000) + 2000
+
+    if (c) {
+        element.style.visibility = 'visible'
+        res.style.display = 'none'
+        inp.style.display = 'none'
+
+        setTimeout(readRes, loadTime)
+    } else {
+        element.style.visibility = 'hidden'
+        res.style.display = 'flex'
+        inp.style.display = 'flex'
+    }
+
+}
+
+function readRes() {
+    load(false)
 
     res.innerHTML = getRes(xv, yv);
     inp.innerHTML = getInp(v, d, a, l);
+
+    temp.setItem('tempR', getRes(xv, yv), 10 + loadTime)
+    temp.setItem('tempI', getInp(v, d, a, l), 10 + loadTime)
 }
 
-window.onload = hoopinate;
+const tempR = temp.getItem('tempR');
+const tempI = temp.getItem('tempI');
+
+if (tempR && tempI != null) {
+    res.innerHTML = tempR
+    inp.innerHTML = tempI
+}
+
+function printContainer(containerId) {
+    const container = document.getElementById(containerId);
+
+    const style = document.createElement('style');
+    style.textContent = `
+        @media print {
+            @page {
+                margin: 0;
+                size: auto;
+            }
+            body * {
+                visibility: hidden;
+            }
+            #${containerId}, #${containerId} * {
+                visibility: visible;
+            }
+            #${containerId} {
+                position: fixed;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                width: 90%;
+            }
+
+            #${containerId} * {
+                color: black !important;
+            }
+        }
+    `;
+    document.head.appendChild(style);
+
+    window.print();
+
+    document.head.removeChild(style);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const calc = document.getElementById('cButton');
+    calc.addEventListener('click', hoopinate);
+
+    const print = document.getElementById('pButton');
+    print.addEventListener('click', function() {
+        printContainer('cont');
+    });
+});
